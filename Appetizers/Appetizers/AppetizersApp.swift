e63cef175358d15161649fb2b80ff84200f9ee12
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-25 22:07:00 +0300

    Add files via upload

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
