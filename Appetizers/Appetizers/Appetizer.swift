85b8c00dceaabdc00b76541e990deb4d78d99d6b
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-28 15:28:04 +0300

    created Model

---
+++ Appetizers/Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 28.11.2022.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct ApptizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let appetizer = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
