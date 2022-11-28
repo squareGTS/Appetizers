05220e4befa240562f98f6f46f69c1462b03883b
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-28 23:03:42 +0300

    errors alerts

---
+++ Appetizers/Appetizers/Alert.swift
@@ -0,0 +1,33 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 28.11.2022.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidURL = AlertItem(title: Text("Server Error"),
+                                              message: Text("The data received from the server was invalid. Please contact support"),
+                                              dismissButton: .default(Text("Ok")))
+    
+    static let invalidResponse = AlertItem(title: Text("Server Error"),
+                                              message: Text("Invalid response from the server. Please try again later or contact support"),
+                                              dismissButton: .default(Text("Ok")))
+    
+    static let invalidData = AlertItem(title: Text("Server Error"),
+                                              message: Text("The was an issue connecting to the server. If this persists, pleasee contact support."),
+                                              dismissButton: .default(Text("Ok")))
+    
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                              message: Text("Unable to complete your request at this time. Please check your internet connection."),
+                                              dismissButton: .default(Text("Ok")))
+}
--- Appetizers/Appetizers/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -10,15 +10,25 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
