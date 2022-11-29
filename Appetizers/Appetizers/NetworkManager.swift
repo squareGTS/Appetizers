2b86e0c4f55cac6731e4e335d4f2011ee7026422
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-29 23:33:20 +0300

    download image network

--- Appetizers/Appetizers/NetworkManager.swift
+++ Appetizers/Appetizers/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Maxim Bekmetov on 28.11.2022.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -44,6 +45,33 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+            
         }.resume()
     }
 }
