32a087be3e8b463d851bbfe976edca7b956e9207
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-29 19:49:14 +0300

    Loading indicator

---
+++ Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 29.11.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
+
