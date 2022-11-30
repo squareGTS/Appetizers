a7e86019074043993f173b792382be9af92bc802
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-30 22:42:32 +0300

    Persisting AppStorage

---
+++ Appetizers/Appetizers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 30.11.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    var birthdate = Date()
+    var extraNapkins = false
+    var frequentRefills = false
+    
+    
+    
+    
+}
--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -15,15 +15,15 @@ struct AccountView: View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Info")) {
-                    TextField("First Name", text: $viewModel.firstName)
-                    TextField("Last Name", text: $viewModel.lastName)
-                    TextField("Email", text: $viewModel.email)
+                    TextField("First Name", text: $viewModel.user.firstName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
+                    TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
                     
                     DatePicker("Birthday",
-                               selection: $viewModel.birthdate,
+                               selection: $viewModel.user.birthdate,
                                displayedComponents: .date)
                     
                     Button {
@@ -34,12 +34,15 @@ struct AccountView: View {
                 }
                 
                 Section(header: Text("Requests")) {
-                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
-                    Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("😀 Account")
+        }
+        .onAppear {
+            viewModel.retrieveUser()
         }
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title,
