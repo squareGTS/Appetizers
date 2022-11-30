c0d3eb4037431bfd6d809e5c0cba9103133561fb
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-30 20:54:58 +0300

    account view model and validation

---
+++ Appetizers/Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  AccountViewModel.swift
+//  Appetizers
+//
+//  Created by Maxim Bekmetov on 30.11.2022.
+//
+
+import SwiftUI
+
+final class AccountViewModel: ObservableObject {
+    
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var email = ""
+    @Published var birthdate = Date()
+    @Published var extraNapkins = false
+    @Published var frequentRefills = false
+    
+    @Published var alertItem: AlertItem?
+    
+    var isValidForm: Bool {
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
+            alertItem = AlertContext.invalidForm
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            return false
+        }
+        return true
+    }
+    
+    func saveChanges() {
+        guard isValidForm else { return }
+        
+        print("Changes have been saved successfully")
+    }
+}
--- Appetizers/Appetizers/Utilities/Alerts/Alert.swift
+++ Appetizers/Appetizers/Utilities/Alerts/Alert.swift
@@ -15,6 +15,7 @@ struct AlertItem: Identifiable {
 }
 
 struct AlertContext {
+    //MARK - Network Alerts
     static let invalidURL = AlertItem(title: Text("Server Error"),
                                               message: Text("The data received from the server was invalid. Please contact support"),
                                               dismissButton: .default(Text("Ok")))
@@ -27,7 +28,17 @@ struct AlertContext {
                                               message: Text("The was an issue connecting to the server. If this persists, pleasee contact support."),
                                               dismissButton: .default(Text("Ok")))
     
+   
     static let unableToComplete = AlertItem(title: Text("Server Error"),
                                               message: Text("Unable to complete your request at this time. Please check your internet connection."),
+                                              dismissButton: .default(Text("Ok")))
+    //MARK - Account Alerts
+    static let invalidForm = AlertItem(title: Text("Invalid Form"),
+                                              message: Text("Please ensure all fields in the form have been field out."),
+                                              dismissButton: .default(Text("Ok")))
+    
+ 
+    static let invalidEmail = AlertItem(title: Text("Invalid Email"),
+                                              message: Text("Please ensure your email is correct."),
                                               dismissButton: .default(Text("Ok")))
 }
