13f7d0e6c88d33a9a2a0d57eba74b5e60dd67a4d
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-01-25 21:04:50 +0300

    refactoring image

--- Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
+++ Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
@@ -13,10 +13,24 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            AppetizerRemoateImage(urlString: appetizer.imageURL)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 90)
-                .cornerRadius(8)
+//            AppetizerRemoateImage(urlString: appetizer.imageURL)
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 120, height: 90)
+//                .cornerRadius(8)
+            
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            } placeholder: {
+                Image("food-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+        }
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
