0d5249add860f53d7c7cd3d1cb000cb1b62c785b
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-01-25 20:23:48 +0300

    add async/await

--- Appetizers/Appetizers/Utilities/Managers/NetworkManager.swift
+++ Appetizers/Appetizers/Utilities/Managers/NetworkManager.swift
@@ -16,37 +16,42 @@ final class NetworkManager {
     
     private init() {}
     
-    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
-        guard let url = URL(string: appetizerURL) else {
-            completed(.failure(.invalidURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            if let _ = error {
-                completed(.failure(.unableToComplete))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(.failure(.invalidResponse))
-                return
-            }
-            
-            guard let data = data else {
-                completed(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let decodedResponse = try decoder.decode(ApptizerResponse.self, from: data)
-                completed(.success(decodedResponse.request))
-            } catch {
-                completed(.failure(.invalidData))
-            }
-        }.resume()
-    }
+//    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+//        guard let url = URL(string: appetizerURL) else {
+//            completed(.failure(.invalidURL))
+//            return
+//        }
+//
+//        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+//            if let _ = error {
+//                completed(.failure(.unableToComplete))
+//                return
+//            }
+//
+//            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//                completed(.failure(.invalidResponse))
+//                return
+//            }
+//
+//            guard let data = data else {
+//                completed(.failure(.invalidData))
+//                return
+//            }
+//
+//            do {
+//                let decoder = JSONDecoder()
+//                let decodedResponse = try decoder.decode(ApptizerResponse.self, from: data)
+//                completed(.success(decodedResponse.request))
+//            } catch {
+//                completed(.failure(.invalidData))
+//            }
+//        }.resume()
+//    }
+    
+    
+    
+    
+    
     
     func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
         
